056e8d69e348c56086e1fd33e839d77860bfc7a4
Artur Gajewski <arturgaj6@wp.pl>
2022-10-17 09:07:35 +0200

    delete <p> from index.js in Section components

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,19 +2,15 @@ import "./style.css"
 const Section = ({title,body,tasksHiddenButtons}) => (
    <section>
     <fieldset className="form__fieldset--list">
-          <p>
             <label className="form__label--list">
               {title}
               {tasksHiddenButtons}
             </label>
-          </p>
         </fieldset>
         <fieldset className="form__fieldset form__fieldset--listTask">
-          <p>
             <label className="form__label">
              {body}
             </label>
-          </p>
         </fieldset>
         </section>
 );
