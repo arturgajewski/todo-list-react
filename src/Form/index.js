6e815cc12a6471c97fcf301852c247afe20806bb
Artur Gajewski <arturgaj6@wp.pl>
2022-10-17 09:08:42 +0200

    added <form> to component and change name autoFocus

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,19 +1,20 @@
 import "./style.css";
 const Form = () => (
-    
+  <form>
     <fieldset className="form__fieldset--list">
       <p className="form__legend--paragraph">
         <input
           className="form__input"
           name="task"
           placeholder="co mam do zrobienia?"
-          autofocus
-          minlength="2"
+          autoFocus
+          minLength="2"
           required
         />
         <button className="button__task--addNewTask">Dodaj zadanie</button>
       </p>
     </fieldset>
+    </form>
 
 );
 
