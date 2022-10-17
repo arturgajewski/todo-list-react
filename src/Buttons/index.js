d6087c6f2cd96456da9d83c2ffa02d78fb8af43e
Artur Gajewski <arturgaj6@wp.pl>
2022-10-17 09:43:31 +0200

    add and use react fragment

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,20 +1,22 @@
 import "./style.css";
 
 const Buttons = ({tasks,hideDoneTasks}) => (
-     tasks.length > 0 && (
-    <div className="button__item">
-      <button className=" button__item">
+     <div className="button__item">
+ {tasks.length > 0 && (
+  <>
+<button className=" button__item">
         {hideDoneTasks ? "Pokaż " : "Ukryj "}ukończone
       </button>
       <button
       className="button__item"
-      disabled={tasks.every(({done}) => done)}
-        
+      disabled={tasks.every(({done}) => done)}  
       >
         Ukończ wszystkie
       </button>
+  </>
+ )}
     </div>
-  )
+  
 );
 
 export default Buttons;
