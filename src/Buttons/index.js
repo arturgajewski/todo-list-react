67a553c536507eb332c3ebe2d7e85ed819f10eaf
Artur Gajewski <arturgaj6@wp.pl>
2022-10-17 13:33:27 +0200

    formatted and rename class in Buttons/index.js

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,22 +1,24 @@
 import "./style.css";
 
-const Buttons = ({tasks,hideDoneTasks}) => (
-     <div className="button__item">
- {tasks.length > 0 && (
-  <>
-<button className=" button__item">
-        {hideDoneTasks ? "Pokaż " : "Ukryj "}ukończone
-      </button>
-      <button
-      className="button__item"
-      disabled={tasks.every(({done}) => done)}  
-      >
-        Ukończ wszystkie
-      </button>
-  </>
- )}
-    </div>
-  
+const Buttons = ({ tasks, hideDoneTasks }) => (
+  <div className="buttons">
+    {tasks.length > 0 && (
+      <>
+        <button className=" buttons__item">
+          {hideDoneTasks 
+          ? "Pokaż " 
+          : "Ukryj "} ukończone
+        </button>
+        <button
+          className="buttons__item"
+          disabled={
+            tasks.every(({ done }) => done)
+          }>
+          Ukończ wszystkie
+        </button>
+      </>
+    )}
+  </div>
 );
 
 export default Buttons;
